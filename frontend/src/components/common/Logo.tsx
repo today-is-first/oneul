baf053bf94f86fd5616398a73ddbb9f76c12787a
today-is-first <diligent0644@gmail.com>
2025-04-24 18:21:35 +0900

    design: 로고 텍스트 퍼블리싱

--- frontend/src/components/common/Logo.tsx
+++ frontend/src/components/common/Logo.tsx
@@ -1,7 +1,12 @@
-import logo from '@imgs/oneulLogo.png';
+import logo from '@imgs/logo.png';
 
 function Logo() {
-  return <img src={logo} alt="logo" />;
+  return (
+    <div className="flex items-center gap-1 cursor-pointer">
+      <img className="w-[40px] h-[40px]" src={logo} alt="logo"></img>
+      <span className="text-xl font-bold text-text-white">Oneul</span>
+    </div>
+  );
 }
 
 export default Logo;
