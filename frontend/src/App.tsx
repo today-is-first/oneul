af8c03e2e5fbfd1c95cbe9890656a3dd0a336de4
today-is-first <diligent0644@gmail.com>
2025-04-24 19:02:38 +0900

    design: navbar 퍼블리싱

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,11 @@ import './App.css';
 import Header from '@components/common/Header';
 import Logo from './components/common/Logo';
 import LoginBtn from './components/common/LoginBtn';
-
+import NavBar from './components/common/NavBar';
 function App() {
   return (
     <div className="w-full h-full">
-      <Header left={<Logo />} center={<div>Center</div>} right={<LoginBtn />} />
+      <Header left={<Logo />} center={<NavBar />} right={<LoginBtn />} />
     </div>
   );
 }
