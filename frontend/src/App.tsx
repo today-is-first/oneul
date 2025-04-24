dc203ee8b85d11dd3422c123244b5aae4bfff7f7
today-is-first <diligent0644@gmail.com>
2025-04-24 19:50:08 +0900

    feat: 라우팅 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
-import Header from '@components/common/Header';
-import Logo from './components/common/Logo';
-import LoginBtn from './components/common/LoginBtn';
-import NavBar from './components/common/NavBar';
+import { Routes, Route } from 'react-router-dom';
+import Home from '@components/home/Home';
+import LoginPage from '@components/login/LoginPage';
+
 function App() {
   return (
-    <div className="w-full h-full">
-      <Header left={<Logo />} center={<NavBar />} right={<LoginBtn />} />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
