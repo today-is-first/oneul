b102edf12ea674cc999f4493a797b46760ce2bae
today-is-first <diligent0644@gmail.com>
2025-04-24 21:17:07 +0900

    feat: 로그인 페이지에서 회원가입 페이지 이동 버튼 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/regist" element={<RegistPage />} />
+        <Route path="/signup" element={<RegistPage />} />
       </Routes>
     </div>
   );
--- frontend/src/components/login/SigninForm.tsx
+++ frontend/src/components/login/SigninForm.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router';
+
 function SigninForm() {
   return (
     <section className="flex flex-col items-center justify-center mt-10 text-logo-white bg-background w-[400px] h-[460px] rounded-lg p-8">
@@ -23,6 +25,9 @@ function SigninForm() {
           로그인
         </button>
       </form>
+      <Link to="/signup">
+        <span className="text-sm text-gray-400">회원가입으로 이동하기</span>
+      </Link>
     </section>
   );
 }
