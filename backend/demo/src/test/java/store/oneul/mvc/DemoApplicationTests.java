9a6834f5d575e23527d3377df4fdeb2c3030d97e
박현호 <parkhh17@naver.com>
2025-04-25 16:56:20 +0900

    chore: 백엔드 폴더 추가

---
+++ backend/demo/src/test/java/store/oneul/mvc/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package store.oneul.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
