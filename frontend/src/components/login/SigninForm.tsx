5ce003e94837af8b7b6af8df57a2e5acc063d18b
today-is-first <diligent0644@gmail.com>
2025-04-29 10:31:47 +0900

    chore: css 순서 변경

--- frontend/src/components/login/SigninForm.tsx
+++ frontend/src/components/login/SigninForm.tsx
@@ -9,14 +9,14 @@ function SigninForm() {
             <span className="text-lg font-bold">아이디</span>
             <input
               type="text"
-              className="focus:ring-point w-full rounded-lg border border-[#333] bg-[#1B1B1E] px-4 py-3 focus:ring-2 focus:outline-none"
+              className="focus:ring-point w-full rounded-lg border border-[#333] bg-[#1B1B1E] px-4 py-3 focus:outline-none focus:ring-2"
             />
           </div>
           <div className="flex w-full flex-col justify-center gap-2">
             <span className="text-lg font-bold">비밀번호</span>
             <input
               type="password"
-              className="focus:ring-point w-full rounded-lg border border-[#333] bg-[#1B1B1E] px-4 py-3 focus:ring-2 focus:outline-none"
+              className="focus:ring-point w-full rounded-lg border border-[#333] bg-[#1B1B1E] px-4 py-3 focus:outline-none focus:ring-2"
             />
           </div>
         </div>
