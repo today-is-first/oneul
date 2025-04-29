2b21813d8e7132f7fd3b4a3bdf014c617a963d34
today-is-first <diligent0644@gmail.com>
2025-04-29 17:03:21 +0900

    chore: 챌린지 룸 생성 페이지 헤더 위치 안바뀌게 변경

--- frontend/src/components/challengeCreate/ChallengeCreatePage.tsx
+++ frontend/src/components/challengeCreate/ChallengeCreatePage.tsx
@@ -5,12 +5,16 @@ import NavBar from "@components/common/NavBar";
 import LoginBtn from "@components/common/LoginBtn";
 
 function ChallengeCreatePage() {
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-bl from-[#21414F] via-[#10212b] to-[#17171C] select-none">
-      <Header left={<Logo />} center={<NavBar />} right={<LoginBtn />} />
-      <ChallengeRoomForm />
-    </div>
-  );
-}
-
-export default ChallengeCreatePage;
+    return (
+      <div className="min-h-screen flex flex-col items-center bg-gradient-to-bl from-[#21414F] via-[#10212b] to-[#17171C] select-none">
+        <Header left={<Logo />} center={<NavBar />} right={<LoginBtn />} />
+  
+        <div className="flex flex-1 w-full items-center justify-center">
+          <ChallengeRoomForm />
+        </div>
+      </div>
+    );
+  }
+  
+  export default ChallengeCreatePage;
+  
