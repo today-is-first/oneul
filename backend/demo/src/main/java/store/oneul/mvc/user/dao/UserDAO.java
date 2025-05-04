5bd1a35c41d766a6f78e482881211212cf65835b
강설민 <svlmin77@gmail.com>
2025-05-04 18:01:21 +0900

    feat: 사용자 DAO 인터페이스 정의

--- backend/demo/src/main/java/store/oneul/mvc/user/dao/UserDAO.java
+++ backend/demo/src/main/java/store/oneul/mvc/user/dao/UserDAO.java
@@ -1,5 +1,12 @@
 package store.oneul.mvc.user.dao;
 
+
+import store.oneul.mvc.user.dto.UserDTO;
+
 public interface UserDAO {
+    UserDTO findByEmail(String email);
+    void insertUser(UserDTO user);
+    void updateUserInfo(UserDTO user); // 닉네임, 전화번호 입력용
+    UserDTO findById(Long userId);
 
 }
