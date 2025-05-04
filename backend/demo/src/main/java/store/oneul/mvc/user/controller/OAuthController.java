daa488984194225c709b8733d541fd137bc576c7
강설민 <svlmin77@gmail.com>
2025-05-04 18:38:31 +0900

    feat: /api/users/signup API 구현 (추가 정보 저장)

--- backend/demo/src/main/java/store/oneul/mvc/user/controller/OAuthController.java
+++ backend/demo/src/main/java/store/oneul/mvc/user/controller/OAuthController.java
@@ -1,15 +1,34 @@
 package store.oneul.mvc.user.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import store.oneul.mvc.user.dto.UserDTO;
+import store.oneul.mvc.user.service.UserService;
 
 @RestController
 @RequestMapping("/oauth")
 public class OAuthController {
 	
 	
-	
+    @Autowired
+    private UserService userService;
+
+    // 1. 회원가입 추가 정보 입력
+    @PutMapping("/signup")
+    public ResponseEntity<String> completeSignup(@AuthenticationPrincipal UserDTO user,
+                                                 @RequestBody UserDTO input) {
+    	user.setUsername(input.getUsername()); 
+    	user.setNickname(input.getNickname());
+        user.setUserTel(input.getUserTel());
+        userService.updateUserInfo(user); // signup_completed = true 포함되게
+        return ResponseEntity.ok("회원가입 완료");
+    }
 	
 	
 
