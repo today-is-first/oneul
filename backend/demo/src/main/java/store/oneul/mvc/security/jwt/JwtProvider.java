6396f718bfa41c5c4855b748adcfabbd5cc3168c
강설민 <svlmin77@gmail.com>
2025-05-04 22:25:37 +0900

    chore: secretkey 디코더변경 및 accesstoken 15분 수정

--- backend/demo/src/main/java/store/oneul/mvc/security/jwt/JwtProvider.java
+++ backend/demo/src/main/java/store/oneul/mvc/security/jwt/JwtProvider.java
@@ -17,8 +17,10 @@ public class JwtProvider {
     @Value("${jwt.secret}")
     private String secretKeyStr;
 
+    private static final long ACCESS_TOKEN_VALIDITY = 15 * 60 * 1000L;
+    
     private SecretKey getSecretKey() {
-        byte[] keyBytes = secretKeyStr.getBytes(); // 또는 Base64 디코딩: Base64.getDecoder().decode(secretKeyStr);
+    	byte[] keyBytes = Base64.getDecoder().decode(secretKeyStr);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
@@ -26,7 +28,7 @@ public class JwtProvider {
         return Jwts.builder()
                 .setSubject(String.valueOf(userId)) // userId를 문자열로 변환해 subject에 저장
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 3600000))
+                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALIDITY))
                 .signWith(getSecretKey())
                 .compact();
     }
