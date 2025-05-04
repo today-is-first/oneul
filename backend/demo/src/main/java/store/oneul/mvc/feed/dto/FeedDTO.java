31cc0e12dd091ca16bf880228195254ce0d2159e
today-is-first <diligent0644@gmail.com>
2025-05-04 20:02:09 +0900

    feat: FeedDTO 구현

---
+++ backend/demo/src/main/java/store/oneul/mvc/feed/dto/FeedDTO.java
@@ -0,0 +1,26 @@
+package store.oneul.mvc.feed.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FeedDTO {
+    private Long id;
+    private Long userId;
+    private Long challengeId;
+    private String imageUrl;
+    private String content;
+    private int likeCount;
+    private LocalDateTime createdAt;
+    private String checkStatus;
+    private LocalDateTime checkedAt;
+}
