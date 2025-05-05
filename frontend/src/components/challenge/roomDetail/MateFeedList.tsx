a85a62e0f09a03204f6daeccb60721d5f3157f3c
aoooec <foottable@gmail.com>
2025-05-05 18:50:10 +0900

    feat: FeedDetailModal, FeedCreateModal 연결 및 썸네일 img태그로 수정

--- frontend/src/components/challenge/roomDetail/MateFeedList.tsx
+++ frontend/src/components/challenge/roomDetail/MateFeedList.tsx
@@ -3,13 +3,18 @@ import MateFeedItem from "./MateFeedItem";
 
 interface MateFeedListProps {
   logs: CheckInLog[];
+  onItemClick?: (log: CheckInLog) => void;
 }
 
-function MateFeedList({ logs }: MateFeedListProps) {
+function MateFeedList({ logs, onItemClick }: MateFeedListProps) {
   return (
     <ul className="h-full space-y-4 overflow-y-auto pr-5">
       {logs.map((log) => (
-        <MateFeedItem key={log.id} log={log} />
+        <MateFeedItem
+          key={log.id}
+          log={log}
+          onClick={() => onItemClick?.(log)}
+        />
       ))}
     </ul>
   );
