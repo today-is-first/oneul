7e54ec6e3f3bab6371820e0b1195efa4f4a5c68b
강설민 <svlmin77@gmail.com>
2025-05-06 20:37:06 +0900

    feat: Redis 기반 토큰 저장소 구현

---
+++ backend/demo/src/main/java/store/oneul/mvc/security/rtr/RefreshTokenDao.java
@@ -0,0 +1,30 @@
+package store.oneul.mvc.security.rtr;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.Duration;
+
+@Repository
+@RequiredArgsConstructor
+public class RefreshTokenDao  {
+
+    private final RedisTemplate<String, String> redisTemplate;
+    private static final String PREFIX = "RT:";
+
+//    // 저장
+//    public void save(String userId, String refreshToken, Duration expiration) {
+//        redisTemplate.opsForValue().set(PREFIX + userId, refreshToken, expiration);
+//    }
+
+    // 조회
+    public String findByUserId(String userId) {
+        return redisTemplate.opsForValue().get(PREFIX + userId);
+    }
+
+    // 삭제
+    public void delete(String userId) {
+        redisTemplate.delete(PREFIX + userId);
+    }
+}
