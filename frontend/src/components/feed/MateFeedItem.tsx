0a321734cc9d7c2f71e12c60474e7e39149b008a
aoooec <foottable@gmail.com>
2025-05-07 17:56:47 +0900

    feat: checkInLog 인터페이스 수정 및 mock 데이터 교체체

--- frontend/src/components/feed/MateFeedItem.tsx
+++ frontend/src/components/feed/MateFeedItem.tsx
@@ -1,3 +1,4 @@
+import { formatTimeAgo } from "@/utils/date";
 import { CheckInLog } from "../challengeDetail/ChallengeFeed";
 
 export interface MateFeedItemProps {
@@ -11,7 +12,7 @@ function MateFeedItem({ log, onClick }: MateFeedItemProps) {
       <a href="#" className="flex items-center gap-4">
         <img
           className="w-26 h-20 rounded-sm"
-          src={log.imageUrl}
+          src={log.image_url}
           alt="오늘 챌린지메이트 인증"
         />
         <div className="flex h-[76px] w-[130px] flex-1 flex-col justify-between">
@@ -19,8 +20,8 @@ function MateFeedItem({ log, onClick }: MateFeedItemProps) {
             {log.content}
           </div>
           <div className="flex justify-between gap-4 text-sm">
-            <div className="text-gray-500">{log.userId}</div>
-            <div className="text-gray-500">{log.date}</div>
+            <div className="text-gray-500">{log.user_id}</div>
+            <div className="text-gray-500">{formatTimeAgo(log.created_at)}</div>
           </div>
         </div>
       </a>
