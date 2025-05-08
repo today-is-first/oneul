d80355ff00368a622122b50c4453d48838a56f20
today-is-first <diligent0644@gmail.com>
2025-05-08 09:08:17 +0900

    faet: Socket.io 서버 서비스 구현

---
+++ backend/demo/src/main/java/store/oneul/mvc/chat/service/SocketIoService.java
@@ -0,0 +1,17 @@
+package store.oneul.mvc.chat.service;
+
+import org.springframework.stereotype.Service;
+
+import com.corundumstudio.socketio.SocketIOClient;
+
+@Service
+public class SocketIoService {
+    public void onConnect(SocketIOClient client) {
+        System.out.println("Connected to socket server");
+    }
+
+    public void onDisconnect(SocketIOClient client) {
+        System.out.println("Disconnected from socket server");
+    }
+
+}
