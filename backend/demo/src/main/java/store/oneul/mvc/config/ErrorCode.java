7ebc803d49c756defd56d7d53128fa8174d0aa7b
today-is-first <diligent0644@gmail.com>
2025-05-09 17:01:21 +0900

    feat : ErrorCode ENUM 구현

---
+++ backend/demo/src/main/java/store/oneul/mvc/config/ErrorCode.java
@@ -0,0 +1,10 @@
+package store.oneul.mvc.config;
+
+public enum ErrorCode {
+    INTERNAL_SERVER_ERROR,
+    USER_NOT_FOUND,
+    INVALID_PARAMETER,
+    DUPLICATE_EMAIL,
+    IMAGE_UPLOAD_FAIL
+    // 필요에 따라 추가
+} 
