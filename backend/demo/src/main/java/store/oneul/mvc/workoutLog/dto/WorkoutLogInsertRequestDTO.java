f2ab4619128da6a1c193365134e87a340000c87e
areum <foottable@gmail.com>
2025-05-13 08:36:51 +0900

    feat: WorkoutLogInsertRequestDTO 구현

---
+++ backend/demo/src/main/java/store/oneul/mvc/workoutLog/dto/WorkoutLogInsertRequestDTO.java
@@ -0,0 +1,17 @@
+package store.oneul.mvc.workoutLog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data        
+@Builder     
+@NoArgsConstructor 
+@AllArgsConstructor
+public class WorkoutLogInsertRequestDTO {
+	private Long userId;
+	private Long challengeId;
+	private Long feedId;
+	private String comment;
+}
