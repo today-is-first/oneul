07ca848d02b4008b6f2b165c444b3bfb398b0233
areum <foottable@gmail.com>
2025-05-13 23:04:23 +0900

    chore: useApi 훅 header content type json 추가

--- frontend/src/hooks/useApi.ts
+++ frontend/src/hooks/useApi.ts
@@ -4,6 +4,7 @@ import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // 쿠키 인증 등에 필요
+  headers: { "Content-Type": "application/json" },
 });
 
 // 요청 인터셉터 (토큰 자동 주입)
@@ -59,7 +60,7 @@ export const patch = async <T>(
   body?: any,
   config?: AxiosRequestConfig,
 ): Promise<T> => {
-  const res: AxiosResponse<T> = await api.put(url, body, config);
+  const res: AxiosResponse<T> = await api.patch(url, body, config);
   return res.data;
 };
 
