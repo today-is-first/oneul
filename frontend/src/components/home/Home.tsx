32e379fd705dc34c49d4eaedac3227f1cdb2e763
today-is-first <diligent0644@gmail.com>
2025-05-14 16:42:59 +0900

    feat: CommunityFeed 기능 구현

--- frontend/src/components/home/Home.tsx
+++ frontend/src/components/home/Home.tsx
@@ -1,13 +1,12 @@
 import GoToChallengeCreateButton from "@components/challengeCreate/GoToChallengeCreateButton";
-import ChallengeList from "../challenge/ChallengeList";
-import MyWorkoutDashbaord from "./MyWorkoutDashboard";
+import ChallengeList from "@/components/challenge/ChallengeList";
+import MyWorkoutDashbaord from "@/components/home/MyWorkoutDashboard";
 
 function Home() {
   return (
     <div className="bg-background h-full w-full">
       <div className="flex items-center justify-center">
         <MyWorkoutDashbaord />
-        <div className="flex flex-col items-center justify-center"></div>
       </div>
 
       <div className="flex flex-col items-center justify-center">
