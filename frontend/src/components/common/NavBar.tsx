0f971e599e2acd258c325c1ed59bbce78ef70c3c
today-is-first <diligent0644@gmail.com>
2025-05-14 20:14:30 +0900

    feat: NavBar Link 연결

--- frontend/src/components/common/NavBar.tsx
+++ frontend/src/components/common/NavBar.tsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
+
 function NavBar() {
   return (
-    <ul className="text-logo-white flex h-full w-full min-w-[400px] cursor-pointer items-center justify-between gap-8">
-      <li>홈</li>
-      <li>카테고리</li>
-      <li>커뮤니티</li>
-      <li>마이페이지</li>
+    <ul className="text-logo-white flex h-full w-full min-w-[400px] cursor-pointer justify-evenly gap-8">
+      <Link to="/">
+        <li>홈</li>
+      </Link>
+      <Link to="/challenge/search">
+        <li>카테고리</li>
+      </Link>
+      <Link to="/mypage">
+        <li>마이페이지</li>
+      </Link>
     </ul>
   );
 }
