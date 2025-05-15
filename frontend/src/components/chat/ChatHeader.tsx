b459be427bb0d8d8fc06bb9418c3d1ceab3e8d9b
today-is-first <diligent0644@gmail.com>
2025-05-15 11:05:44 +0900

    feat: 채팅창에 챌린지 상세보기 기능 추가

--- frontend/src/components/chat/ChatHeader.tsx
+++ frontend/src/components/chat/ChatHeader.tsx
@@ -1,7 +1,16 @@
-function ChatHeader({ challengeName }: { challengeName: string }) {
+import ChatBanner from "@/components/chat/ChatBanner";
+
+function ChatHeader({
+  challengeId,
+  challengeName,
+}: {
+  challengeId: number;
+  challengeName: string;
+}) {
   return (
     <div className="bg-black-chat flex h-14 items-center justify-between border-b border-gray-700 px-4">
       <div className="text-lg font-bold text-white">{challengeName}</div>
+      <ChatBanner challengeId={challengeId} />
     </div>
   );
 }
