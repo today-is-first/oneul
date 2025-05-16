7c0b04c22a2b269edfb0b4e06c6d2c527f4da7db
today-is-first <diligent0644@gmail.com>
2025-05-16 10:12:03 +0900

    feat: FeedDAO 피드, 스트릭 기능 추가

--- backend/demo/src/main/java/store/oneul/mvc/feed/dao/FeedDAO.java
+++ backend/demo/src/main/java/store/oneul/mvc/feed/dao/FeedDAO.java
@@ -1,12 +1,15 @@
 package store.oneul.mvc.feed.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import store.oneul.mvc.feed.dto.CommunityFeedDTO;
 import store.oneul.mvc.feed.dto.FeedDTO;
 import store.oneul.mvc.feed.dto.FeedEvaluationRequest;
+import store.oneul.mvc.feed.dto.StreakDTO;
 
 @Mapper
 public interface FeedDAO {
@@ -17,6 +20,12 @@ public interface FeedDAO {
     public void deleteFeed(@Param("challengeId") Long challengeId, @Param("id") Long id);
 
     public FeedDTO getFeed(@Param("challengeId") Long challengeId, @Param("id") Long id);
+
+    public List<FeedDTO> getMyFeeds(@Param("userId") Long userId);
+
+    public List<CommunityFeedDTO> getCommunityFeeds();
+
+    public List<StreakDTO> getStreak(@Param("userId") Long userId);
 
     public List<FeedDTO> getFeeds(@Param("challengeId") Long challengeId);
     
