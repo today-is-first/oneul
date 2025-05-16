4a721b6ae26a28cc7f2fd38143b266e0ee711b84
today-is-first <diligent0644@gmail.com>
2025-05-16 10:12:25 +0900

    feat: Streak, CommunityFeed DTO 추가

---
+++ backend/demo/src/main/java/store/oneul/mvc/feed/dto/StreakDTO.java
@@ -0,0 +1,11 @@
+package store.oneul.mvc.feed.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StreakDTO {
+    private String date;
+    private Integer count;
+} 
