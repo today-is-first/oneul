960db84157945cb80e9a67b9f753f9365d1be785
areum <foottable@gmail.com>
2025-05-21 19:18:26 +0900

    design: 뱃지 스타일 추가

--- frontend/src/components/common/Badge.tsx
+++ frontend/src/components/common/Badge.tsx
@@ -7,18 +7,22 @@ function Badge({ children, type }: BadgeProps) {
   let color;
   switch (type) {
     case "CHALLENGE":
-      color = "bg-[#8B5CF6]/25 text-[#8B5CF6]";
+      color = "bg-primary-purple-200/25 text-primary-purple-200";
       break;
     case "NORMAL":
       color = "bg-gray-500/25 text-gray-300";
       break;
     case "RECRUITING":
-      color = "bg-yellow-100/25 text-amber-700";
+      color = "bg-green-300/15 text-green-600";
+      break;
+    case "ENDED":
+      color = "bg-transparent border border-gray-700 box-border text-gray-400";
+      break;
   }
 
   return (
     <span
-      className={`whitespace-nowrap rounded-md px-2 pb-1 pt-1.5 ${color} inline-flex align-baseline text-sm font-semibold`}
+      className={`bg-prim text-re whitespace-nowrap rounded-md px-2 pb-1 pt-1.5 ${color} inline-flex align-baseline text-sm font-semibold`}
     >
       {children}
     </span>
