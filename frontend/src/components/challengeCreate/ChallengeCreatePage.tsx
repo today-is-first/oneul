e183bf2518b6fab475cb872f8871558e84f173d5
today-is-first <diligent0644@gmail.com>
2025-05-23 11:39:59 +0900

    feat: 챌린지 룸 페이지 레이아웃 수정

--- frontend/src/components/challengeCreate/ChallengeCreatePage.tsx
+++ frontend/src/components/challengeCreate/ChallengeCreatePage.tsx
@@ -1,17 +1,9 @@
-import Header from "@components/common/Header";
-import Logo from "@components/common/Logo";
 import ChallengeRoomForm from "@components/challengeCreate/ChallengeRoomForm";
-import NavBar from "@components/common/NavBar";
-import LoginBtn from "@components/common/LoginBtn";
 
 function ChallengeCreatePage() {
   return (
     <div className="flex min-h-screen select-none flex-col items-center bg-gradient-to-bl from-[#21414F] via-[#10212b] to-[#17171C]">
-      <Header left={<Logo />} center={<NavBar />} right={<LoginBtn />} />
-
-      <div className="flex w-full flex-1 items-center justify-center">
-        <ChallengeRoomForm />
-      </div>
+      <ChallengeRoomForm />
     </div>
   );
 }
