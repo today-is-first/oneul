47eb7f0706925dafa3703aa8a512d1b0158655a1
seolmin <svlmin77@gmail.com>
2025-05-23 17:44:16 +0900

    feat: 유저 결제내역 조회 SQL 쿼리 작성

--- backend/demo/src/main/java/store/oneul/mvc/payment/dao/PaymentDAO.java
+++ backend/demo/src/main/java/store/oneul/mvc/payment/dao/PaymentDAO.java
@@ -1,9 +1,12 @@
 package store.oneul.mvc.payment.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import store.oneul.mvc.payment.dto.CancelFailLogDTO;
 import store.oneul.mvc.payment.dto.PaymentDTO;
+import store.oneul.mvc.payment.dto.PaymentReceiptDTO;
 import store.oneul.mvc.payment.dto.RefundReceiptDTO;
 
 @Mapper
@@ -21,4 +24,6 @@ public interface PaymentDAO {
     RefundReceiptDTO findRefundReceiptByOrderId(String orderId);
     
     CancelFailLogDTO findCancelFailLogByOrderId(String orderId);
+
+	List<PaymentReceiptDTO> findReceiptDTOsByUserId(long userId);
 }
