acb8ec0986438b1027e6052ac9ddb14db434602f
today-is-first <diligent0644@gmail.com>
2025-05-27 16:37:35 +0900

    fix: challengeName => name 으로 수정

--- backend/demo/src/main/java/store/oneul/mvc/payment/dto/PaymentReceiptDTO.java
+++ backend/demo/src/main/java/store/oneul/mvc/payment/dto/PaymentReceiptDTO.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 @Data
 public class PaymentReceiptDTO {
     private Long paymentId;
-    private String challengeName;     // JOIN으로 받아온 값
+    private String name;     // JOIN으로 받아온 값
     private LocalDateTime updatedAt;  // 결제 완료 시점
     private int amount;               // 결제 금액
     private String status;            // PAID, REFUNDED 등
